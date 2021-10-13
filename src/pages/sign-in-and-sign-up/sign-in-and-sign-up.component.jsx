d546099c997e33cea4afd1d057f382537a6ca2ae
KOchz <kirill1chernomazov@gmail.com>
2021-10-14 00:06:34 +0300

    added sign-in component and styles

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+import FormInput from '../../components/form-input/form-input.component'
+
+const SingInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export default SingInAndSignUp
